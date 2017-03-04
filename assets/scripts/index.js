d1a96a9d2bb718208888acdc004a5eb77f48e47e
Soboscott <scott.landry01772@yahoo.com>
2017-03-04 15:00:57 -0500

    Fixed endind div fixed handeler

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -15,8 +15,9 @@ $(() => {
 
 // use require without a reference to ensure a file is bundled
 const authEvents = require('./auth/events.js');
-
+const reviewsEvents = require('./reviews/events.js');
 $(() => {
   authEvents.addHandlers();
+  reviewsEvents.addAjaxHandlers();
 });
 require('./example');
